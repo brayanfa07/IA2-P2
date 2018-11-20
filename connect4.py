893d9a0ea84095639ec0e79adbd96dd3ec90d0e4
brayanfa07 <brayan.f.a07@hotmail.com>
2018-11-20 14:53:02 -0600

    Cambios de funciones a clases

--- connect4.py
+++ connect4.py
@@ -1,71 +1,79 @@
-array = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-
-
 class GeneticConnect4(object):
 	"""docstring for GeneticConnect4"""
 	def __init__(self):
 		super(GeneticConnect4, self).__init__()
-		self.arg = arg
+		self.array = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
 
-	def mutation():
+	def mutation(self):
+		#IMPLEMENT THE CODE
 		return 0		
 
 
-	def crossing():
+	def crossing(self):
+		#IMPLEMENT THE CODE
 		return 0
 
 
-	def 
+	def selection(self):
+		#IMPLEMENT THE CODE
+		return 0
 
 
-	def 
-def insert_disc():
-
-	print_grid(array)
-
-	disc = eval(input("INSERTE DONDE VA A COLOCAR LA FICHA --> "))
-
-	if disc > 0 and disc <= 7:	
-		disc = disc - 1
-
-		rowsCount = len(array)
-		columnsCount = len(array[0])
-
-		actualPosition = rowsCount - 1
-		inserted = False
-
-		while actualPosition >= 0 :
-			if array[actualPosition][disc] == 0 and inserted == False:
-				array[actualPosition][disc] = 1
-				inserted = True
-			else:
-				actualPosition -= 1
-		return insert_disc()
-	else:
-		print("Posición equivocada")
-		return insert_disc()
+	def fitness_funtion(self):
+		#IMPLEMENT THE CODE	
+		return 0
 
 
-def print_grid(array):
+	def insert_disc(self):
 
-	rowsCount = len(array)
-	columnsCount = len(array[0])
+		self.print_grid()
 
-	print()
-	for i in range(rowsCount):
-		row = ""
-		for j in range(columnsCount):
-			if array[i][j] == 0:
-				row = row + " "
-			elif array[i][j] == 1:
-				row = row + "1"
-			else:
-				row = row + "2"
-		print("| " + row+ " |")
+		disc = eval(input("INSERTE DONDE VA A COLOCAR LA FICHA --> "))
 
-	print("|_________|") 
-	print()
+		if disc > 0 and disc <= 7:	
+			disc = disc - 1
 
-	return 0
+			rows_count = len(self.array)
+			columns_count = len(self.array[0])
 
-insert_disc() 
+			actualPosition = rows_count - 1
+			inserted = False
+
+			while actualPosition >= 0 :
+				if self.array[actualPosition][disc] == 0 and inserted == False:
+					self.array[actualPosition][disc] = 1
+					inserted = True
+				else:
+					actualPosition -= 1
+			return self.insert_disc()
+		else:
+			print("Posición equivocada")
+			return self.insert_disc()
+
+
+	def print_grid(self):
+
+		rows_count = len(self.array)
+		columns_count = len(self.array[0])
+
+		print()
+		for i in range(rows_count):
+			row = ""
+			for j in range(columns_count):
+				if self.array[i][j] == 0:
+					row = row + " "
+				elif self.array[i][j] == 1:
+					row = row + "1"
+				else:
+					row = row + "2"
+			print("| " + row+ " |")
+
+		print("|_________|") 
+
+		print()
+
+		return 0
+
+
+new_game = GeneticConnect4()
+new_game.insert_disc()
