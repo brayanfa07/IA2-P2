2b0f39d03d524a8422765203d6621f8f033b2fc7
brayanfa07 <brayan.f.a07@hotmail.com>
2018-11-20 14:06:17 -0600

    Definición esqueleto de funciones

--- connect4.py
+++ connect4.py
@@ -1,25 +1,49 @@
-array = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,1,0,0,0,1]]
+array = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
 
+
+class GeneticConnect4(object):
+	"""docstring for GeneticConnect4"""
+	def __init__(self):
+		super(GeneticConnect4, self).__init__()
+		self.arg = arg
+
+	def mutation():
+		return 0		
+
+
+	def crossing():
+		return 0
+
+
+	def 
+
+
+	def 
 def insert_disc():
 
 	print_grid(array)
 
 	disc = eval(input("INSERTE DONDE VA A COLOCAR LA FICHA --> "))
-	disc = disc - 1
 
-	rowsCount = len(array)
-	columnsCount = len(array[0])
+	if disc > 0 and disc <= 7:	
+		disc = disc - 1
 
-	actualPosition = rowsCount - 1
-	inserted = False
+		rowsCount = len(array)
+		columnsCount = len(array[0])
 
-	while actualPosition >= 0 :
-		if array[actualPosition][disc] == 0 and inserted == False:
-			array[actualPosition][disc] = 1
-			inserted = True
-		else:
-			actualPosition -= 1
-	return insert_disc()
+		actualPosition = rowsCount - 1
+		inserted = False
+
+		while actualPosition >= 0 :
+			if array[actualPosition][disc] == 0 and inserted == False:
+				array[actualPosition][disc] = 1
+				inserted = True
+			else:
+				actualPosition -= 1
+		return insert_disc()
+	else:
+		print("Posición equivocada")
+		return insert_disc()
 
 
 def print_grid(array):
@@ -27,6 +51,7 @@ def print_grid(array):
 	rowsCount = len(array)
 	columnsCount = len(array[0])
 
+	print()
 	for i in range(rowsCount):
 		row = ""
 		for j in range(columnsCount):
@@ -38,8 +63,9 @@ def print_grid(array):
 				row = row + "2"
 		print("| " + row+ " |")
 
-	print("|_________|")
+	print("|_________|") 
+	print()
 
 	return 0
 
-insert_disc()
+insert_disc() 
